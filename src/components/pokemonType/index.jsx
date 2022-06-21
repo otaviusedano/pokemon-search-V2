f3f5fe35e3f34146166d0c2bfcfeb162a6b52227
Otávio <sedanootavio@gmail.com>
2022-06-21 11:54:24 -0300

    refactor: pokemonType recfactored

--- src/components/pokemonType/index.jsx
+++ src/components/pokemonType/index.jsx
@@ -4,9 +4,9 @@ import typeColors from '../../core/services/pokemonTypes'
 
 import './styles.scss'
 
-function PokemonTypes (poke) {
-  const pokemonTypes = poke?.info?.types?.map((types, index) => {
-    const typeName = types?.type?.name
+function PokemonTypes ({ info }) {
+  const pokemonTypes = info.types?.map((types, index) => {
+    const typeName = types.type.name
 
     return (
       <div style={{ backgroundColor: typeColors[types?.type?.name] }} key={index} className='types'>
@@ -16,7 +16,7 @@ function PokemonTypes (poke) {
   })
 
   return (
-    <div className='container__types'>
+    <div className='container-types'>
       {pokemonTypes}
     </div>
   )
