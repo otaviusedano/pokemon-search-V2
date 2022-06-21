35566eea6f4fac17cc6dd95c8c6f2b899e481f2d
Otávio <sedanootavio@gmail.com>
2022-06-21 12:05:40 -0300

    feat: add details route and notFound page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Details from './pages/details'
 import Home from './pages/home'
+import NotFound from './pages/notFound'
 
 function App () {
   return (
    <>
-    <BrowserRouter>
+    <Router>
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/details/:pokemonName' element={<Details />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
    </>
   )
 }
