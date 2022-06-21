347a4462e32accaf263dc182feb871a9bd7f686c
Otávio <sedanootavio@gmail.com>
2022-06-21 12:02:05 -0300

    refactor: api refactored

--- src/core/api/index.js
+++ src/core/api/index.js
@@ -1,12 +1,13 @@
 const apiUrl = 'https://pokeapi.co/api/v2/'
 
 const getName = async (pokemonName) => {
-  const res = await fetch(apiUrl + `pokemon/${pokemonName}`)
+  const pokemonNameAjusted = pokemonName.toLowerCase().trim(' ')
+  const res = await fetch(apiUrl + `pokemon/${pokemonNameAjusted}`)
   const body = await res.json()
   return body
 }
 
-export const getAll = async (page = 0, limit = 20) => {
+export const getAll = async (page = 0, limit = 24) => {
   const res = await fetch((apiUrl + `pokemon?limit=${limit}&offset=${page}`))
   const body = await res.json()
   return body
