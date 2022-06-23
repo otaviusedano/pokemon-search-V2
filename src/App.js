f2c9d5959f782b6a9b59d993a656bc49f61aec29
Otávio <sedanootavio@gmail.com>
2022-06-23 19:06:10 -0300

    feat: add context pokemon provider

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { PokemonContextProvider } from './contexts/pokemonContext'
 import Details from './pages/details'
 import Home from './pages/home'
 import NotFound from './pages/notFound'
@@ -7,13 +8,15 @@ import NotFound from './pages/notFound'
 function App () {
   return (
    <>
-    <Router>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/details/:pokemonName' element={<Details />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
+    <PokemonContextProvider>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/details/:pokemonName' element={<Details />} />
+          <Route path="/*" element={<NotFound />} />
+        </Routes>
+      </Router>
+    </PokemonContextProvider>
    </>
   )
 }
