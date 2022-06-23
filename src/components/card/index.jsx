ad8645a09b81014c4a42f352ca0790d6d222b0ca
Otávio <sedanootavio@gmail.com>
2022-06-23 19:16:30 -0300

    feat: add principal infos of pokemon and refactored component

--- src/components/card/index.jsx
+++ src/components/card/index.jsx
@@ -3,18 +3,18 @@ import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import getPokemonByName from '../../core/helper/getPokemonByName'
+import PrincipalInfos from '../principalDetails'
 
 import './styles.scss'
-import PokemonTypes from '../pokemonType/index'
 
 function Card ({ pokemonName }) {
   const [pokemon, setPokemon] = useState([])
 
-  useEffect(() => {
-    const pokemonInfos = async () => {
-      setPokemon(await getPokemonByName(pokemonName))
-    }
+  const pokemonInfos = async () => {
+    setPokemon(await getPokemonByName(pokemonName))
+  }
 
+  useEffect(() => {
     pokemonInfos()
   }, [pokemonName])
 
@@ -22,12 +22,7 @@ function Card ({ pokemonName }) {
     <Link to={`/details/${pokemonName}`} >
       <div className='container-card'>
         <div className='card'>
-          <div className='card-top'>
-            <span>#{pokemon.id}</span>
-            <h1> {pokemonName} </h1>
-          </div>
-          <img src={pokemon?.sprites?.front_default} alt="pokemonImage" />
-          <PokemonTypes info={pokemon} />
+          <PrincipalInfos pokemon={pokemon} />
         </div>
       </div>
     </Link>
