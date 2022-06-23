587994c5eed664ed511b2ef8bb08eb944b051976
Otávio <sedanootavio@gmail.com>
2022-06-23 19:04:30 -0300

    feat: add modifier stats

--- src/core/helper/pokemonMapper.js
+++ src/core/helper/pokemonMapper.js
@@ -2,17 +2,26 @@ import React from 'react'
 
 import typeColors from '../../core/services/pokemonTypes'
 
-export const pokemonStats = (pokemon) => pokemon.stats?.map((type, index) => {
-  const stats = type.base_stat
+const pokemonStatsMapper = (pokemon) => pokemon.stats?.map((type, index) => {
+  const statName = type.stat.name
+  const statBase = type.base_stat
+  const statNameSplited = statName.split('-')
+  const statNameRefact = statNameSplited.map((name) => {
+    const statNameSubStr = name.substring(0, 3)
+    return statNameSubStr
+  })
+  const statNameJoin = statNameRefact.join('-')
+
   return (
     <div key={index}>
-      <h2>{stats}</h2>
+      <h2>{statNameJoin}</h2>
+      <h2>{statBase}</h2>
     </div>
   )
 })
 
-export const pokemonTypes = (pokemon) => pokemon?.types?.map((types, index) => {
-  const typeName = types?.type?.name
+const pokemonTypesMapper = (pokemon) => pokemon?.types?.map((types, index) => {
+  const typeName = types.type.name
 
   return (
     <div style={{ backgroundColor: typeColors[types?.type?.name] }} key={index} className='types'>
@@ -20,3 +29,5 @@ export const pokemonTypes = (pokemon) => pokemon?.types?.map((types, index) => {
     </div>
   )
 })
+
+export { pokemonStatsMapper, pokemonTypesMapper }
