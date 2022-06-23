b06f44954dcd126ed274f7f4eedf4841abfb6f38
Otávio <sedanootavio@gmail.com>
2022-06-23 18:58:49 -0300

    feat: add pokemon context

---
+++ src/contexts/pokemonContext.js
@@ -0,0 +1,87 @@
+import React, { createContext, useState, useEffect } from 'react'
+
+import { getAll } from './../core/api/index'
+import getPokemonByName from './../core/helper/getPokemonByName'
+
+import Card from '../components/card'
+
+export const PokemonContext = createContext()
+
+export function PokemonContextProvider ({ children }) {
+  const [pokemonViewState, setPokemonViewState] = useState([])
+  const [pokemonFinded, setPokemonFinded] = useState([])
+  const [offset, setOffset] = useState(0)
+  const [pokemonsTotal, setPokemonsTotal] = useState(0)
+  const [filterName, setFilterName] = useState('')
+  const [result, setResult] = useState(false)
+  const defaultLimit = 24
+
+  const getPokemons = async (countInit) => {
+    const res = await getAll(countInit)
+    const pokemons = await res.results
+    setPokemonViewState(pokemons)
+    setPokemonsTotal(res.count)
+    setResult(false)
+  }
+
+  useEffect(() => {
+    getPokemons(offset)
+  }, [offset, !filterName.length])
+
+  const searchPokemon = async () => {
+    const pokemon = await getPokemonByName(filterName)
+    if (pokemon === 'Error') {
+      setPokemonFinded('Pokemon notFound!')
+    } else {
+      setPokemonFinded(pokemon)
+    }
+    setResult(true)
+  }
+
+  const handleSubmit = (e) => e.preventDefault()
+
+  const filterPokemons = () => {
+    const pokemonFilter = filterName.toLowerCase()
+    return pokemonViewState.filter((pokemon) =>
+      pokemon.name.toLowerCase().includes(pokemonFilter.trim(' '))
+    )
+  }
+
+  const pokemonFiltered = filterPokemons().map((pokemon, index) => (
+    <div key={index}>
+      <Card pokemonName={pokemon.name} />
+    </div>
+  ))
+
+  const pokemonResult = pokemonFinded !== 'Pokemon notFound!'
+    ? <div>
+        <Card pokemonName={pokemonFinded.name} />
+      </div>
+    : <h1> Pokemon Não Encontrado </h1>
+
+  return (
+    <PokemonContext.Provider value={
+      {
+        pokemonViewState,
+        setPokemonViewState,
+        pokemonFinded,
+        setPokemonFinded,
+        result,
+        setResult,
+        offset,
+        setOffset,
+        filterName,
+        setFilterName,
+        pokemonFiltered,
+        pokemonsTotal,
+        setPokemonsTotal,
+        searchPokemon,
+        pokemonResult,
+        defaultLimit,
+        handleSubmit
+      }
+    }>
+      {children}
+    </PokemonContext.Provider>
+  )
+}
