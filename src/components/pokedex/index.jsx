1ef3f3304f7cdd110c5a3028f12f757c2a66d164
Otávio <sedanootavio@gmail.com>
2022-06-23 19:22:09 -0300

    feat: add context pokemon

--- src/components/pokedex/index.jsx
+++ src/components/pokedex/index.jsx
@@ -1,42 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useContext } from 'react'
 
-import { getAll } from '../../core/api'
-import Card from '../card/index'
+import { PokemonContext } from '../../contexts/pokemonContext'
 
 import './styles.scss'
 
-function Pokedex ({ total, pokemonFinded, result, setResult, pokemonsPerPage, filter }) {
-  const [pokemonViewState, setPokemonViewState] = useState([])
-
-  const getPokemons = async (amount) => {
-    const res = await getAll(amount)
-    const pokemons = await res.results
-    setPokemonViewState(pokemons)
-    total(res.count)
-    setResult(pokemonFinded.length)
-  }
-
-  useEffect(() => {
-    getPokemons(pokemonsPerPage)
-  }, [pokemonsPerPage, filter.length === 0])
-
-  const filterPokemons = () => {
-    const pokemonFilter = filter.toLowerCase()
-    return pokemonViewState.filter((pokemon) =>
-      pokemon.name.toLowerCase().includes(pokemonFilter.trim(' '))
-    )
-  }
-
-  const pokemonFiltered = filterPokemons().map((pokemon, index) => (
-    <div key={index}>
-      <Card pokemonName={pokemon.name} />
-    </div>
-  ))
-
-  const pokemonResult =
-    <div>
-      <Card pokemonName={pokemonFinded.name} />
-    </div>
+function Pokedex () {
+  const { pokemonFiltered, result, pokemonResult } = useContext(PokemonContext)
 
   return (
     <section>
