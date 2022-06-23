c70b891104c72aeb7acea3d9cadf4a87f7748ca2
Otávio <sedanootavio@gmail.com>
2022-06-23 19:14:40 -0300

    feat: add component in page

--- src/pages/details/index.jsx
+++ src/pages/details/index.jsx
@@ -1,44 +1,14 @@
-import React, { useEffect, useState } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import React from 'react'
 import ContainerPage from '../../components/containerPage'
+import PokemonDetailsCard from '../../components/pokemonDetailsCard'
 
-import getPokemonName from '../../core/helper/getPokemonByName'
-import { pokemonStats, pokemonTypes } from '../../core/helper/pokemonMapper'
+import './styles.scss'
 
 function Details () {
-  const params = useParams()
-  const pokemonParams = params.pokemonName
-
-  const [pokemon, setPokemon] = useState([])
-
-  const pokemonInfos = async () => {
-    const infos = await getPokemonName(pokemonParams)
-    setPokemon(infos)
-    console.log(infos)
-  }
-
-  useEffect(() => {
-    pokemonInfos()
-  }, [])
-
   return (
     <ContainerPage display={'flex'}>
       <main>
-        <section>
-          <Link to='/' >Voltar</Link>
-          <div>
-            {pokemon.id}
-          </div>
-          <div>
-            <h1>{pokemon.name}</h1>
-          </div>
-          <div>
-            {pokemonStats(pokemon)}
-          </div>
-          <div>
-            {pokemonTypes(pokemon)}
-          </div>
-        </section>
+        <PokemonDetailsCard />
       </main>
     </ContainerPage>
   )
