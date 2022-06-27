95fbb1fd3effc0c5a0a877b13a322c298197493b
Otávio <sedanootavio@gmail.com>
2022-06-27 11:00:17 -0300

    feat: add not found in pokemon img

--- src/components/principalDetails/index.jsx
+++ src/components/principalDetails/index.jsx
@@ -1,19 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import PokemonTypes from '../pokemonTypes/index'
+
+import getPokemonByName from '../../core/helper/getPokemonByName'
 
 import './styles.scss'
 
 function PrincipalInfos ({ pokemon }) {
+  const [notFoundImg, setNotFoundImg] = useState([])
   const pokemonImg = pokemon?.sprites?.front_default
   const pokemonName = pokemon.name
   const pokemonId = pokemon.id
+  const dittoImg = async () => {
+    const ditto = await getPokemonByName('ditto')
+    setNotFoundImg(ditto?.sprites?.front_default)
+  }
+
+  useEffect(() => {
+    dittoImg()
+  }, [])
 
   return (
     <div className='container-infos'>
       <span>#{pokemonId}</span>
       <h1> {pokemonName} </h1>
-      <img src={pokemonImg} alt="pokemonImage" />
+      <img src={pokemonImg || notFoundImg} alt="pokemonImage" />
     <PokemonTypes pokemon={pokemon} />
     </div>
   )
